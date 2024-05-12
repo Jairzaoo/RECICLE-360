ccb81abad19c09191ee30062f0800f411d806ed2
Gustavo Rezende <38204615+Jairzaoo@users.noreply.github.com>
2024-05-11 23:10:29 -0300

    Add files via upload

--- projetointegrador/projetointegrador/settings.py
+++ projetointegrador/projetointegrador/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'projetointegrador.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {        
-        'ENGINE': 'django.db.backends.sqlite3',        
-        'NAME': BASE_DIR / 'db.sqlite3',    }   
-    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',  # Your database name
+        'USER': 'default',    # Your database user
+        'PASSWORD': 'R0P5fomkbZzM',  # Your database password
+        'HOST': 'ep-withered-wood-a4k8zdbu-pooler.us-east-1.aws.neon.tech',  # Your database host
+        'PORT': '5432',       # Your database port
+    }
 }
 
 
