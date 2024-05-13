ca8f48b4a13c3202f3ff63b44d51b2dd403bfa44
Gustavo Rezende <38204615+Jairzaoo@users.noreply.github.com>
2024-05-13 12:23:31 -0300

    1

--- projetointegrador/projetointegrador/settings.py
+++ projetointegrador/projetointegrador/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/img'
 
 # Directory where collectstatic will gather static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
